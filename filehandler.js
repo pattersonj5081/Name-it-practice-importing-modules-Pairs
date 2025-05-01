edefbab340fd471c879cc2980ec73c4ab513dc66
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-04-30 21:53:38 -0500

    changes I implemented

--- filehandler.js
+++ filehandler.js
@@ -1,18 +1,25 @@
-const { JSON } = require("stream/consumers");
 const taskManager = require("./task-manager.js"); 
 const fs = require("fs"); 
+/*const tasks = {
+  description: "Walk the dog", 
+  priority: "high",
+  isComplete: true
+};*/
 
-function saveTasks(filePath, tasks){
-fs.writeFileSync(filePath, (tasks))
+function saveTasks(filePath, tasks){ // writes the tasks to a file in JSON format 
+  tasksJSON = JSON.stringify(tasks)
+  fs.writeFileSync(filePath, tasksJSON)
+  console.log(`These tasks have been saved to ${filePath} successfully!`); 
 }; 
 
-function loadTasks (filePath){
-if (fs.existsSync(filePath)){
-  const data =  fs.readFileSync(filePath, "utf-8")
-    return JSON.parse(data); 
-}else{
-return []
-}
-};
+function loadTasks (filePath){ 
+  data = []                                 //Reads and parses the tasks from the file in JSON format 
+  if (fs.existsSync(filePath)){
+        console.log(`The file path: ${filePath} exists!`)            
+        data = fs.readFileSync(filePath, "utf-8");
+        console.log(`Your tasks have been successfully loaded to the ${filePath}`); 
+    }
+  return JSON.parse(data); 
+}; 
 
 module.exports = {saveTasks, loadTasks}; 
